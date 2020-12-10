c2a0cb59a39918c1e7cf7b80c2f82e5f5155a6cd
DoeyJonuts <joeyefarah@gmail.com>
2020-12-10 16:10:47 -0600

    Added Search Any Scenario Button

--- main.js
+++ main.js
@@ -91,3 +91,24 @@ function displayButton(element) {
       }
     }
   }
+
+   /* Any Savestate Button */
+   function anySavestateButton() {
+    document.getElementById("myDropdown AnySavestate").classList.toggle("show");
+  }
+  
+  function filterFunctionAnySavestate() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput AnySavestate");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown AnySavestate");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
